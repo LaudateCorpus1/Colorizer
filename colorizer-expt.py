0c824f7c10d9ad0bb094bcee0afcfd365c0f2aa8
Josh Cook <joshuacook0023@gmail.com>
2020-11-02 06:36:45 -0500

    feat: loop over all images in a directory

--- colorizer-expt.py
+++ colorizer-expt.py
@@ -2,24 +2,33 @@ import requests
 import shutil
 from pathlib import Path
 from secrets import DEEPAI_API_KEY
+from os.path import basename, splitext
 
-image_name = "trenchwarfare2-original.jpg"
-output_name = image_name.replace("original", "colorized")
-original_url = Path(f"images/{image_name}")
-output_url = Path(f"images/{output_name}")
 
-r = requests.post(
-    "https://api.deepai.org/api/colorizer",
-    files={
-        'image': open(original_url, 'rb'),
-    },
-    headers={'api-key': DEEPAI_API_KEY}
-)
+def colorize(image_path, save_path, API_KEY):
+    deepai_res = requests.post(
+        "https://api.deepai.org/api/colorizer",
+        files={"image": open(image_path, "rb")},
+        headers={"api-key": API_KEY},
+    )
 
-imageResult = requests.get(r.json()['output_url'], stream=True)
-if imageResult.status_code == 200:
-    with open(output_url, 'wb') as output_file:
-        imageResult.raw.decode_content = True
-        shutil.copyfileobj(imageResult.raw, output_file)
-else:
-    print(f"image result status code: {imageResult.status_code}")
+    colorized_image_url = requests.get(deepai_res.json()["output_url"], stream=True)
+    if colorized_image_url.status_code == 200:
+        with open(save_path, "wb") as save_file:
+            colorized_image_url.raw.decode_content = True
+            shutil.copyfileobj(colorized_image_url.raw, save_file)
+    else:
+        print(f"image result status code: {colorized_image_url.status_code}")
+
+
+images_dir = Path("images")
+original_dir = images_dir / "original-images"
+output_dir = images_dir / "colorized-images"
+
+all_input_images = original_dir.glob("*jpg")
+for input_image in all_input_images:
+    output_name = splitext(basename(input_image))[0] + "_color.jpg"
+    output_path = output_dir / output_name
+    if not output_path.exists():
+        print(f"Colorizing '{basename(input_image)}'...")
+        colorize(input_image, output_path, DEEPAI_API_KEY)
